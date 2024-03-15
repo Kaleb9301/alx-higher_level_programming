4f273b6ec9839f22116194ae3b4fd88bc8189db8
Kaleb Wondwossen <kalebwondwossent@gmail.com>
2024-03-15 01:59:52 +0000

    task 1

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -6,4 +6,4 @@ if __name__ == "__main__":
     print("{:n} + {:n} = {:n}".format(a, b, add(a, b)))
     print("{:n} - {:n} = {:n}".format(a, b, sub(a, b)))
     print("{:n} * {:n} = {:n}".format(a, b, mul(a, b)))
-    print("{:n} / {:n} = {:n}"/format(a, b, div(a, b)))
+    print("{:n} / {:n} = {:n}".format(a, b, div(a, b)))
