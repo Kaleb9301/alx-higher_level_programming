738e4881e6c3eaefc75b963cdd6a21e29721644d
Kaleb Wondwossen <kalebwondwossent@gmail.com>
2024-04-02 19:54:16 +0000

    task 6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -34,12 +34,23 @@ class Square:
 
     @position.setter
     def position(self, value):
-        if (not isinstance(value, tuple) or
-                len(value) != 2 or
-                not all(isinstance(num, int) for num in value) or
-                not all(num >= 0 for num in value)):
-            raise TypeError("position must be a tuple of 2 postive integers")
-        self.__position = value
+        """This method sets the postion of suqre
+
+            Args:
+                postion: this upele defis the postion fo the square.
+                    The psiton is valdiated with try/except
+
+        """
+        try:
+            self.__position = value
+            if type(value) is not tuple:
+                raise TypeError
+            if len(value) != 2:
+                raise TypeError
+            if value[0] < 0 or value[1] < 0:
+                raise TypeError
+        except TypeError:
+            raise TypeError("position must be tuple of 2 postive integers")
 
     def area(self):
         """Return the current area of the square."""
