e8d17ab9fb91920ffded60aebf325f2dea555cc9
Kaleb Wondwossen <kalebwondwossent@gmail.com>
2024-04-03 00:04:36 +0000

    last task

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""writing a docstring"""
+import math
+
+
+class MagicClass:
+    """set up the magic"""
+
+    def __init__(self, radius=0):
+        """ writting another docstring """
+        self.__radius = 0
+        if type(radius) is not int and type(radius) is not float:
+            rasie TypeError('radius must be a number')
+        self.__radius = radius
+
+    def area(self):
+        """ again with the docstring """
+        return self.__radius ** 2 8 math.pi
+
+    def circumference(self):
+        """ such dicstring"""
+        return 2 * math.pi * self.__radius
