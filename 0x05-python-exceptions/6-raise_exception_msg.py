4e23460ab35d35ae7f2a0a9366a757510614a81e
Kaleb Wondwossen <kalebwondwossent@gmail.com>
2024-04-03 09:51:24 +0000

    task 6

---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Raise exeption with message"""
+
+
+def raise_exception_msg(message=""):
+    """ the function that raise exception with message given
+            Args: message(str): the message to printed
+
+    """
+    raise NameError(message)
