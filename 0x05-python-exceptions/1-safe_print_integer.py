658f02539a3c0d9d0c97107f71e62bd93f4d9e6b
Kaleb Wondwossen <kalebwondwossent@gmail.com>
2024-04-03 04:43:53 +0000

    task 1

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3i
+"""This module do print integer and handle
+    exception
+"""
+
+def safe_print_integer(value):
+    """ The function that print the value if the 
+        the value is integer and handle exception
+        Args:
+                value(int): the number to printed
+    """
+
+    try:
+        print("{:d}".format(value))
+        return True
+    except (ValueError, TypeError):
+        return False
