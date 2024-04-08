b9781bc8aff606851464dd780ea3eebf88c0734f
Kaleb Wondwossen <kalebwondwossent@gmail.com>
2024-04-08 10:32:05 +0000

    task 1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -30,7 +30,7 @@ class Rectangle:
         if type(width) is not int:
             raise TypeError("width must be an integer")
         elif width < 0:
-            raise ValueError("wid::th must be >= 0")
+            raise ValueError("width must be >= 0")
         self.__width = width
 
     @property
