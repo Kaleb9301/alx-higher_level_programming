7a4f3dd275da3855d017bb917d6fc69c5ba1722c
Kaleb Wondwossen <kalebwondwossent@gmail.com>
2024-04-08 18:46:36 +0000

    task 9

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -86,7 +86,7 @@ class Rectangle:
         return shape_rep_string[:-1]
 
     def __repr__(self):
-        return "Rectangle ({}, {})".fomate(self.width, self.height)
+        return "Rectangle ({}, {})".format(self.width, self.height)
 
     def __del__(self):
         type(self).number_of_instances -= 1
