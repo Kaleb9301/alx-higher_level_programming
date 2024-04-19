2d062a08e36e5acc4f2dedb7fc16260774b4dfbc
Kaleb Wondwossen <kalebwondwossent@gmail.com>
2024-04-19 05:08:13 +0000

    task 0

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""This module used for print integer"""
+
+
+def print_list_integer(my_list):
+    """The function that print integer
+        Arg: my_list: list of integer to be printed.
+        Return: None
+    """
+    for i in my_list:
+        print(i)
