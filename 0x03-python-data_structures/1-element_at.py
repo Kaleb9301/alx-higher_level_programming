af64256d4d0be40afbc7f2731f16a21c083a9eb1
Kaleb Wondwossen <kalebwondwossent@gmail.com>
2024-04-19 05:26:04 +0000

    task 1

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,12 @@
+#!/ur/bin/python3
+
+"""this module return the element of the list at some index"""
+
+
+def element_at(my_list, indx):
+    """"This function return the at the indx
+        Arg: my_list :the lsit to be used to get the element
+             indx: the position that element found
+        Return: the element at the index
+    """
+    return my_list[indx]
