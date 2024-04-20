1e8bd0206cbba3da6422255da1ccf3100eb65021
Kaleb Wondwossen <kalebwondwossent@gmail.com>
2024-04-20 05:27:41 +0000

    taks 5

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"This module is to return the number of keys in dictionary"""
+
+
+def number_keys(a_dictionary):
+    """This function returns the number of keys in adictionary
+        Arg: a_dictionary: dictionary used to count the number of keys
+        Returns: number of keys in the dictionary
+    """
+    return (len(a_dictionary))
