433b750d776784e33a3aa4fcc63ff33074380bd2
Kaleb Wondwossen <kalebwondwossent@gmail.com>
2024-04-28 06:12:52 +0000

    task 102

--- 0x07-python-test_driven_development/102-python.c
+++ 0x07-python-test_driven_development/102-python.c
@@ -8,26 +8,27 @@
  */
 void print_python_string(PyObject *p)
 {
-	PyObject *str, *repr;
+	long int length;
 
-	(void)repr;
+	fflush(stdout);
+
+
 	printf("[.] string object info\n");
 
-	if (strcmp(p->ob_type_name, "str"))
+	if (strcmp(p->ob_type->tp_name, "str"))
 	{
 		printf("  [ERROR] Invalid String Object\n");
 		return;
 	}
+
+	length = ((PyASCIIObject *)(p))->length;
+
 	if (PyUnicode_IS_COMPACT_ASCII(p))
 		printf("  type: compact ascii\n");
 	else
-		printf(" type: compact unicode object\n");
+		printf("  type: compact unicode object\n");
 
-	repr = Pyobject_Repr(p);
-	str = PyUnicode_AsEncodedString(p, "utf-8", "~E~");
-	repr = Pyobject_Repr(p);
-	str = PyUnicode_AsEncodeString(p, "utf-8", "~E~");
-	printf("  length:%d\n", PyUnicode_GET_SIZE(p));
-	printf("  value: %s\n", PyBytes_AsString(str));
+	printf("  length:%ld\n", PyUnicode_GET_SIZE(p));
+	printf("  value: %ls`\n", PyUnicode_AsWideCharString(p, &length));
 }
 
