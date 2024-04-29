a8f36061bfbea9153cc0c574bfc0ca42c28a2aee
Kaleb Wondwossen <kalebwondwossent@gmail.com>
2024-04-29 08:38:09 +0000

    task 100

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -11,20 +11,26 @@ def matrix_mul(m_a, m_b):
             m_a(list of lists ints/floats): The first matrix.
             m_b(list of lists of ints/floats): The second matrix
         Raise:
+            TypeError: If m_a or m_b aren't a list
+            TypeError: If m_a or m_b aren't a list of a lists
             TypeError: If either m_a or m_b is not a list of ints/floats.
-            TypeError: If either m_a or m_b is empty.
-            TyoeError: If either m_a or m_b has different-sized rows.
+            ValueError: If either m_a or m_b is empty.
+            TypeError: If either m_a or m_b has different-sized rows.
+            TypeError: If the rows of m_a  and m_b don't have the same size
             ValueError: If m_a and m_b cannot be multiplied.
         Returns:
             A new matrix representing the mulitplication of m_a by m_b.
     """
     if not isinstance(m_a, list):
         raise TypeError("m_a must be a list")
+
     if not isinstance(m_b, list):
         raise TypeError("m_b must be a list")
+
     for i in range(len(m_a)):
         if not isinstance(m_a[i], list):
             raise TypeError("m_a must be a list of lists")
+
     for i in range(len(m_b)):
         if not isinstance(m_b[i], list):
             raise TypeError("m_b must be a list of lists")
@@ -42,9 +48,9 @@ def matrix_mul(m_a, m_b):
         raise TypeError("m_b should contain only integers or floats")
 
     if not all(len(row) == len(m_a[0]) for row in m_a):
-        raise TypeError("each row of m_a must should be of the same size")
+        raise TypeError("each row of m_a must be of the same size")
     if not all(len(row) == len(m_b[0]) for row in m_b):
-        raise TypeError("each row of m_b must should be of the same size")
+        raise TypeError("each row of m_b must be of the same size")
 
     if len(m_a[0]) != len(m_b):
         raise ValueError("m_a and m_b can't be multiplied")
