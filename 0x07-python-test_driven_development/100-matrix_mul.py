340c8d9c2073b3b1362be84eda9349e808965680
Kaleb Wondwossen <kalebwondwossent@gmail.com>
2024-04-29 06:54:01 +0000

    task 100

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -22,7 +22,8 @@ def matrix_mul(m_a, m_b):
         raise ValueError("m_a can't be empty")
     if m_b == [] or m_b == [[]] or m_b == [[], []]:
         raise ValueError("m_b can't be empty")
-
+    if [] in m_a:
+        raise ValueError("m_a can't be empty")
 
     if not isinstance(m_a, list):
         raise TypeError("m_a must be a list")
