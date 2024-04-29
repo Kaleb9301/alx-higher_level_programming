3d4fbae94d51ac3b8b8f1946c451b22a92e8188d
Kaleb Wondwossen <kalebwondwossent@gmail.com>
2024-04-29 07:06:19 +0000

    task 100

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -18,11 +18,6 @@ def matrix_mul(m_a, m_b):
         Returns:
             A new matrix representing the mulitplication of m_a by m_b.
     """
-    if m_a == [] or m_a == [[], []] or m_a == [[]]:
-        raise ValueError("m_a can't be empty")
-    if m_b == [] or m_b == [[]] or m_b == [[], []]:
-        raise ValueError("m_b can't be empty")
-
     if not isinstance(m_a, list):
         raise TypeError("m_a must be a list")
     if not isinstance(m_b, list):
