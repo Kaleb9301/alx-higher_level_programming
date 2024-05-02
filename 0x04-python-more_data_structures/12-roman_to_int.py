a69cc5ae0e5dc383d9799e9d97adf94c0a814256
Kaleb Wondwossen <kalebwondwossent@gmail.com>
2024-05-02 07:06:26 +0000

    task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,7 @@
 
 
 def roman_to_int(roman_string):
-    if not roman_string or type(roman_string) is str:
+    if not roman_string or type(roman_string) is not str:
         return 0
     total = 0
     digits = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
